f987504dc21eef65c35caa784a45de40f871ba27
zqh <zqh123196@gmail.com>
2020-01-04 01:13:23 +0800

    update  图文列表

--- flutter/hello_world/lib/main.dart
+++ flutter/hello_world/lib/main.dart
@@ -18,13 +18,32 @@ class APP extends StatelessWidget {
 
 class Home extends StatelessWidget {
   Widget _listItemBuilder(BuildContext context, int index) {
-    return Text(posts[index].title);
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0,)
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+        backgroundColor: Colors.grey[100],
         appBar: AppBar(
           title: Text('Hello Flutter!'),
           elevation: 10, // 阴影
