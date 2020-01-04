52382a4e3c6feda397a110560f4e457963cfb649
zqh <zqh123196@gmail.com>
2020-01-04 15:54:42 +0800

    tab page

---
+++ flutter/hello_world/lib/demo/listview.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+
+
+class Listview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Hello Flutter!'),
+          elevation: 10, // 阴影
+        ),
+        // body: Hello(),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ),
+      );
+  }
+
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0,)
+        ],
+      ),
+    );
+  }
+}
--- flutter/hello_world/lib/main.dart
+++ flutter/hello_world/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+// import 'demo/listview.dart';
 
 main() => runApp(APP());
 
@@ -11,67 +11,62 @@ class APP extends StatelessWidget {
       home: Home(),
       theme: ThemeData(
         primaryColor: Colors.green,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5), 
+        splashColor: Colors.red[300], // 水波纹
       ),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16.0,)
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
           title: Text('Hello Flutter!'),
           elevation: 10, // 阴影
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Menu",
+            onPressed: () => debugPrint("Menu button is on pressed."),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "Search",
+              onPressed: () => debugPrint("Search button is on pressed."),
+            ),
+            IconButton(
+              icon: Icon(Icons.more_horiz),
+              tooltip: "more_horiz",
+              onPressed: () => debugPrint("More_horiz button is on pressed."),
+            ),
+          ],
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54, // 指示器(下面那条横线)的颜色
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+              
+            ],
+          ),
         ),
-        // body: Hello(),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ),
-      );
-  }
-}
-
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Text(
-        'hello',
-        textDirection: TextDirection.rtl,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.red,
-        ),
-      ),
-    );
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
+        )),
+      )
+    ;
   }
 }
