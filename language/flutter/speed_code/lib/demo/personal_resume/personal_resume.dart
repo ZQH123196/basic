0821b4e998b0255b6e739250f18b87f7edfd62bc
zqh <zqh123196@gmail.com>
2020-01-31 23:59:41 +0800

    update

--- language/flutter/speed_code/lib/demo/personal_resume/personal_resume.dart
+++ language/flutter/speed_code/lib/demo/personal_resume/personal_resume.dart
@@ -4,6 +4,46 @@
  */
 import 'package:flutter/material.dart';
 
+class ProfileFourPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.more_vert, color: Colors.black,),onPressed: (){},)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 80.0,
+                  child: Image.asset("lib/demo/travel_home_page/assets/images/1.jpg"),),
+                SizedBox(width: 20.0,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: <Widget>[
+                    Text("阿斯顿马丁"),
+                    Text("骑士"),
+                    Text("喜欢呼吸空气"),
+                  ],
+
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class PersonalResume extends StatefulWidget {
   @override
   State createState() {
@@ -19,11 +59,7 @@ class _PersonalResume_State extends State<PersonalResume> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-      ),
-    );
+    return null;
   }
 
 
