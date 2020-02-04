6b169c149e9d67e0d11c38bcac83ef80421dcd1c
zqh <zqh123196@gmail.com>
2020-02-05 04:00:58 +0800

    update

--- snake/Python3/main.py
+++ snake/Python3/main.py
@@ -1,45 +1,69 @@
-# # # snack : <===, < header, === body
-# # # f -> food
-# # # 请确认控制台使用的是等宽字体
+# # snack : <===, < header, === body
+# # f -> food
+# # 请确认控制台使用的是等宽字体
 
-# import sys
+import sys
+import time
 
-# surroundChar = '-'
+surroundChar = '-'
 
-# def buildMap(mapSize: tuple, ceilSize: int, gap: int = 0):
-#     print('build func!')
-#     row = mapSize[0]
-#     col = mapSize[1]
-#     _str = ""
-#     # + 2 是因为需要绘制边框，上下两条，左右两条
-#     for _row in range(row + 2):
-#         if _row > 0 and _row < (row+2-1):
-#             _str = _str + surroundChar + (' ' * col) + surroundChar
-#         else: # 边框绘制
-#             _str = _str + (surroundChar * (col + 2))
-#         _str = _str + '\n'
-#     pass
-#     print(_str)
-
-# def refreshMap():
-#     pass
-
-
-
-# if __name__ == '__main__':
-#     row = 15
-#     col = 15
-#     mapSize = (row, col)
+def printCanvas(canvas):
+    for _str in canvas:
+        print(_str)
     
-#     ceilSize = 10
 
-#     buildMap(mapSize, ceilSize)
-#     print('snake main file is running!')
+def buildMap(mapSize: tuple, ceilSize: int, gap: int = 0):
+    print('build func!')
+    row = mapSize[0]
+    col = mapSize[1]
+    canvas = []
+    # + 2 是因为需要绘制边框，上下两条，左右两条
+    for _row in range(row + 2):
+        if _row > 0 and _row < (row+2-1):
+            canvas.append(surroundChar + (' ' * col) + surroundChar)
+        else: # 边框绘制
+           canvas.append(surroundChar * (col + 2))
+    pass
+    return canvas
 
-# import time
-# for i in range(10):
-#     time.sleep(0.2) 
-#     print ("Loading... ".format(i)+str(i), end="\r")
+
+def refreshMap():
+    pass
+
+def snakeRun(canvas):
+    header = ">"
+    body = "="*3
+    snake = body + header
+    # 由于蛇的身子非常有规律，蛇走的时候，其身子必定会顺着头走过的地方一一走过
+    # 或者说除头部外某个身体的下一个移动位置必定是前一个身段所处的位置，而头部的下一个位置为玩家控制
+    # snake 设计一个与头身一一对应的队列，当头移动时，数组最前面进一个头部运动的新坐标，头部所对应的位置就自动更新，且后续所有的身子都会更新
+    canvas[1] = canvas[1][:1] + snake + canvas[1][len(snake)+1:]
+
+    # for i in range(0, 5):
+    #     print(" "*i + snake, end='\r', flush=True)
+    #     time.sleep(0.1)
+
+def controle():
+    pass
+
+if __name__ == '__main__':
+    row = 15
+    col = 15
+    mapSize = (row, col)
+    
+    ceilSize = 10
+
+    canvas = buildMap(mapSize, ceilSize)
+    # printCanvas(canvas)
+    snakeRun(canvas)
+    printCanvas(canvas)
+    
+    print(canvas[1][:])
+    # print('snake main file is running!')
+
+
+
+
 
 
 # import time
@@ -58,17 +82,17 @@
 # answer = input(prompt)
 # t.cancel()
 
-import threading
+# import threading
 
-def input_func( context ):
-    context[ 'data' ] = input( 'input:' )
+# def input_func( context ):
+#     context[ 'data' ] = input( 'input:' )
 
-context = { 'data' : 'default' }
-t = threading.Thread( target = input_func, args = ( context , ), daemon=True)
-t.start( )
-t._stop()
-t.join( 3 ) #等待 5 秒
-print( context ) 
+# context = { 'data' : 'default' }
+# t = threading.Thread( target = input_func, args = ( context , ), daemon=True)
+# t.start( )
+# t._stop()
+# t.join( 3 ) #等待 5 秒
+# print( context ) 
 
 # import os
 # i=os.system("cls")
