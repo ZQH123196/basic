5281dc98f4b61f1090fb4828af75908a3b14dc56
zqh <zqh123196@gmail.com>
2022-01-22 01:12:56 +0800

    update

---
+++ Vue3/multiple-tag/src/views/components/SideBar/components/SideBarIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+const props = defineProps<{
+    text: string
+}>()
+
+
+
+</script>
+
+
+<template>
+    <div class="group">
+        {{props.text}}
+        <div class="siderbar-tooltip group-hover:scale-100">tooltip 💡</div>
+    </div>
+</template>
---
+++ Vue3/multiple-tag/src/views/dashboarder/dashboarder.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SideBar from "../components/SideBar/SideBar.vue"
+
+
+
+</script>
+
+
+<template>
+    <SideBar />
+</template>
